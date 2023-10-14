6b147ba64490533d5c15e337c671d693f736b735
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-10-14 13:59:06 +0700

    update solution for problem 70

--- Daily/70. Climbing Stairs.ts
+++ Daily/70. Climbing Stairs.ts
@@ -1,1 +1,8 @@
-71. Simplify Path
+function climbStairs(n: number): number {
+    const dp: number[] = Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+    
+    for (let i = 2; i <= n; i++) dp[i] = dp[i - 1] + dp[i - 2];
+    return dp[n];
+};
