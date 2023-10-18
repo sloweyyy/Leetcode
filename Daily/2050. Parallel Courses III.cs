dbfa2d66eae35bd192e03d1e8b66d60e6cd184f0
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-10-18 22:33:36 +0700

    Update 2050. Parallel Courses III.cs

--- Daily/2050. Parallel Courses III.cs
+++ Daily/2050. Parallel Courses III.cs
@@ -2,59 +2,44 @@ public class Solution
 {
     public int MinimumTime(int n, int[][] relations, int[] time)
     {
-        // Build the graph
-        List<int>[] graph = new List<int>[n];
+        List<List<int>> graph = new List<List<int>>();
         for (int i = 0; i < n; i++)
         {
-            graph[i] = new List<int>();
+            graph.Add(new List<int>());
         }
-        int[] indegrees = new int[n];
+
         foreach (int[] relation in relations)
         {
             int prev = relation[0] - 1;
             int next = relation[1] - 1;
-            graph[prev].Add(next);
-            indegrees[next]++;
+            graph[next].Add(prev);
         }
 
-        // Topological sorting
-        Queue<int> queue = new Queue<int>();
-        for (int i = 0; i < n; i++)
+        int[] memo = new int[n];
+        int overallMinTime = 0;
+
+        for (int course = 0; course < n; course++)
         {
-            if (indegrees[i] == 0)
-            {
-                queue.Enqueue(i);
-            }
-        }
-        int[] order = new int[n];
-        int index = 0;
-        while (queue.Count > 0)
-        {
-            int curr = queue.Dequeue();
-            order[index++] = curr;
-            foreach (int next in graph[curr])
-            {
-                indegrees[next]--;
-                if (indegrees[next] == 0)
-                {
-                    queue.Enqueue(next);
-                }
-            }
+            overallMinTime = Math.Max(overallMinTime, CalculateTime(course, graph, time, memo));
         }
 
-        // Calculate the minimum time required
-        int[] maxTime = new int[n];
-        int result = 0;
-        foreach (int curr in order)
+        return overallMinTime;
+    }
+
+    private int CalculateTime(int course, List<List<int>> graph, int[] time, int[] memo)
+    {
+        if (memo[course] != 0)
         {
-            int currTime = time[curr];
-            foreach (int prev in graph[curr])
-            {
-                currTime = Math.Max(currTime, maxTime[prev] + time[curr]);
-            }
-            maxTime[curr] = currTime;
-            result = Math.Max(result, currTime);
+            return memo[course];
         }
-        return result;
+
+        int maxPrerequisiteTime = 0;
+        foreach (int prereq in graph[course])
+        {
+            maxPrerequisiteTime = Math.Max(maxPrerequisiteTime, CalculateTime(prereq, graph, time, memo));
+        }
+
+        memo[course] = maxPrerequisiteTime + time[course];
+        return memo[course];
     }
 }
