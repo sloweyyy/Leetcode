37e87df5a4ff09ac90f8d369aa4717544ef13ba7
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-10-29 16:11:13 +0700

    Create 458. Poor Pigs.py

---
+++ Daily/458. Poor Pigs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        tests = minutesToTest // minutesToDie
+        pigs = 0
+        while (tests + 1) ** pigs < buckets:
+            pigs += 1
+        return pigs
