e66573380488d8ba520888d5439e06ee15f69655
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-11-12 01:00:09 +0700

    Update 2642. Design Graph With Shortest Path Calculator.cpp

--- Daily/2642. Design Graph With Shortest Path Calculator.cpp
+++ Daily/2642. Design Graph With Shortest Path Calculator.cpp
@@ -1,40 +1,52 @@
-class Graph {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Graph
+{
 public:
     vector<vector<pair<int, int>>> adjList;
-    Graph(int n, vector<vector<int>>& edges) {
+    Graph(int n, vector<vector<int>> &edges)
+    {
         adjList.resize(n);
-        for (auto& e: edges)
+        for (auto &e : edges)
             adjList[e[0]].push_back(make_pair(e[1], e[2]));
     }
 
-    void addEdge(vector<int> edge) {
+    void addEdge(vector<int> edge)
+    {
         adjList[edge[0]].push_back(make_pair(edge[1], edge[2]));
     }
 
-    int shortestPath(int node1, int node2) {
+    int shortestPath(int node1, int node2)
+    {
         int n = adjList.size();
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
         vector<int> costForNode(n, INT_MAX);
         costForNode[node1] = 0;
         pq.push({0, node1});
 
-        while (!pq.empty()) {
+        while (!pq.empty())
+        {
             int currCost = pq.top()[0];
             int currNode = pq.top()[1];
             pq.pop();
 
-            if (currCost > costForNode[currNode]) {
+            if (currCost > costForNode[currNode])
+            {
                 continue;
             }
-            if (currNode == node2) {
+            if (currNode == node2)
+            {
                 return currCost;
             }
-            for (auto& neighbor : adjList[currNode]) {
+            for (auto &neighbor : adjList[currNode])
+            {
                 int neighborNode = neighbor.first;
                 int cost = neighbor.second;
                 int newCost = currCost + cost;
 
-                if (newCost < costForNode[neighborNode]) {
+                if (newCost < costForNode[neighborNode])
+                {
                     costForNode[neighborNode] = newCost;
                     pq.push({newCost, neighborNode});
                 }
