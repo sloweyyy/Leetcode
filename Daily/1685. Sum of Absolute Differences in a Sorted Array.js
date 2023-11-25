66e3b1c4647d424dc31dcd66deaa19b63c2af622
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-11-26 01:17:31 +0700

    Create 1685. Sum of Absolute Differences in a Sorted Array.js

---
+++ Daily/1685. Sum of Absolute Differences in a Sorted Array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getSumAbsoluteDifferences = function(nums) {
+    let n = nums.length;
+    let sum = 0;
+    let res = [];
+    for (let i = 0; i < n; i++) {
+        sum += nums[i];
+    }
+    let leftSum = 0;
+    for (let i = 0; i < n; i++) {
+        sum -= nums[i];
+        res[i] = nums[i] * i - leftSum + sum - nums[i] * (n - i - 1);
+        leftSum += nums[i];
+    }
+    return res;
+};
