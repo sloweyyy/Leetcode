2fc7913217d345f8372ca4188dc43b22da717c1b
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2024-01-06 18:56:30 +0700

    Update 3 solution

---
+++ Daily/300. Longest Increasing Subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * len(nums)
+        res = 0
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]: 
+                    dp[i] = max(dp[i], dp[j] + 1)
+            res = max(res, dp[i])
+        return res
