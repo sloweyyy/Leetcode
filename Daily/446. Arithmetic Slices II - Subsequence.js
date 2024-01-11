92fb89b7f19b155bd1c7532efdb6052ab7fe4d6b
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2024-01-12 01:20:42 +0700

    Update

---
+++ Daily/446. Arithmetic Slices II - Subsequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numberOfArithmeticSlices = function(nums) {
+    const dp = [];
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+        dp[i] = new Map();
+        for (let j = 0; j < i; j++) {
+            const diff = nums[i] - nums[j];
+            const prev = dp[j].get(diff) || 0;
+            const cur = dp[i].get(diff) || 0;
+            dp[i].set(diff, prev + cur + 1);
+            total += prev;
+        }
+    }
+    return total;
+};
