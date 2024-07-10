02aee093df50e6b51d48d2fa8f188073e1139b50
slowey <truonglevinhphuc2006@gmail.com>
2024-07-10 23:38:51 +0700

    Create 1598. Crawler Log Folder.js

---
+++ Daily/1598. Crawler Log Folder.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function(logs) {
+    let depth = 0;
+    for (let i = 0; i < logs.length; i++) {
+        if (logs[i] === '../') {
+            if (depth > 0) {
+                depth--;
+            }
+        } else if (logs[i] !== './') {
+            depth++;
+        }
+    }
+    return depth;
+};
