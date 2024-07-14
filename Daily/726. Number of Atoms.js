9957ee0bc62bc518d0eb597e77d0b86c251696f2
slowey <truonglevinhphuc2006@gmail.com>
2024-07-14 11:38:44 +0700

    140724

---
+++ Daily/726. Number of Atoms.js
@@ -0,0 +1,60 @@
+/**
+ * @param {string} formula
+ * @return {string}
+ */
+const countOfAtoms = (formula) => {
+    const stack = [];
+    let currentMap = {};
+    let index = 0;
+
+    const readNextElement = (i) => {
+        if (!formula[i].match(/[A-Z]/)) return null;
+        let element = formula[i++];
+        while (formula[i]?.match(/[a-z]/)) {
+            element += formula[i++];
+        }
+        return [element, i];
+    };
+
+    const readNextDigit = (i) => {
+        if (!formula[i]?.match(/[0-9]/)) return [1, i];
+        let number = 0;
+        while (formula[i]?.match(/[0-9]/)) {
+            number = number * 10 + Number(formula[i++]);
+        }
+        return [number, i];
+    };
+
+    while (index < formula.length) {
+        if (formula[index] === "(") {
+            stack.push(currentMap);
+            currentMap = {};
+            index++;
+        } else if (formula[index] === ")") {
+            const [multiplier, newIndex] = readNextDigit(++index);
+            index = newIndex;
+            Object.keys(currentMap).forEach(
+                (key) => (currentMap[key] *= multiplier)
+            );
+            const lastMap = stack.pop() || {};
+            Object.keys(currentMap).forEach((key) => {
+                lastMap[key] = (lastMap[key] || 0) + currentMap[key];
+            });
+            currentMap = lastMap;
+        } else {
+            const [element, newIndex] = readNextElement(index);
+            index = newIndex;
+            const [count, newI] = readNextDigit(index);
+            index = newI;
+            currentMap[element] = (currentMap[element] || 0) + count;
+        }
+    }
+
+    return Object.entries(currentMap)
+        .sort((a, b) => a[0].localeCompare(b[0]))
+        .reduce(
+            (result, [element, count]) =>
+                `${result}${element}${count === 1 ? "" : count}`,
+            ""
+        );
+};
