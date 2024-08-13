dab3f605d0a49fae39c9ec60efef99bc3bb42a9c
slowey <truonglevinhphuc2006@gmail.com>
2024-08-13 20:25:24 +0700

    feat: Implement combinationSum2 function for finding combinations that sum up to a target

--- Daily/40. Combination Sum II.js
+++ Daily/40. Combination Sum II.js
@@ -3,12 +3,10 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum2 = function(candidates, target) {
-
+var combinationSum2 = function (candidates, target) {
     const result = [];
 
     const dfs = (index, sum, path) => {
-
         if (sum === target) {
             result.push(path);
             return;
@@ -19,14 +17,12 @@ var combinationSum2 = function(candidates, target) {
         }
 
         for (let i = index; i < candidates.length; i++) {
-
             if (i > index && candidates[i] === candidates[i - 1]) {
                 continue;
             }
 
             dfs(i + 1, sum + candidates[i], [...path, candidates[i]]);
         }
-
     };
 
     candidates.sort((a, b) => a - b);
