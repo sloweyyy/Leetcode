9d31bb0a42c65dd2b12172c2aba573c9f011bf62
slowey <truonglevinhphuc2006@gmail.com>
2024-08-23 16:31:18 +0700

    chore: Add solution for "Fraction Addition and Subtraction" problem

---
+++ Daily/592. Fraction Addition and Subtraction.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} expression
+ * @return {string}
+ */
+var fractionAddition = function (expression) {
+    // -1/3-1/2
+    let arr = expression.split(/(?=[-+])/); // ["-1/3", "-1/2"]
+    let res = [0, 1]; // [0, 1]
+    for (let i = 0; i < arr.length; i++) {
+        // i = 0  // i = 1
+        let [num, den] = arr[i].split("/"); // num = -1, den = 3 // num = -1, den = 2
+        let [a, b] = res; // a = 0, b = 1 // a = -1, b = 3
+        let gcd = getGCD(b, den); // gcd = 3 // gcd = 1
+        res = [(a * den) / gcd + (b * num) / gcd, (b * den) / gcd]; // res = [-1, 3] // res = [-5, 6]
+        let g = getGCD(Math.abs(res[0]), res[1]); // g = 1 // g = 1
+        res = [res[0] / g, res[1] / g]; // res = [-1, 3] // res = [-5, 6]
+    }
+    return res.join("/"); // "-5/6"
+};
+
+var getGCD = function (a, b) {
+    return b === 0 ? a : getGCD(b, a % b);
+};
+
+console.log(fractionAddition("-1/3-1/2")); // "-5/6"
