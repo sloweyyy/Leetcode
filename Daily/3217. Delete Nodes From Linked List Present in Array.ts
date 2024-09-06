168d303ff124554e93153f55bb2089833bd1335a
sloweyyy <truonglevinhphuc2006@gmail.com>
2024-09-06 21:35:32 +0700

    feat: Add missingRolls function for finding missing observations
    feat: Add modifiedList function for deleting nodes from linked list

---
+++ Daily/3217. Delete Nodes From Linked List Present in Array.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function modifiedList(nums: number[], head: ListNode | null): ListNode | null {
+    const set = new Set(nums);
+    let dummy = new ListNode(0, head);
+    let current = dummy;
+    while (current.next) {
+        if (set.has(current.next.val)) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return dummy.next;
+};
