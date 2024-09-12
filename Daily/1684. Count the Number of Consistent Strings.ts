d6c0d40be8591a0acc15c5cbc0e5e4e773d0b7b9
sloweyyy <truonglevinhphuc2006@gmail.com>
2024-09-13 02:06:56 +0700

    feat: Add countConsistentStrings function for counting the number of consistent strings
    Add a new function called countConsistentStrings that takes in an allowed string and an array of words. The function counts the number of consistent strings, which are strings that only contain characters from the allowed string. It uses a set to efficiently check if each character in a word is allowed. The result is the length of the filtered array of consistent strings.

---
+++ Daily/1684. Count the Number of Consistent Strings.ts
@@ -0,0 +1,4 @@
+function countConsistentStrings(allowed: string, words: string[]): number {
+    const allowedSet = new Set(allowed.split(''));
+    return words.filter(word => word.split('').every(c => allowedSet.has(c))).length;
+};
