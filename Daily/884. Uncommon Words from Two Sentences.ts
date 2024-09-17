f50ab76198d25d4ab5faba90dfbccfc08ee9b609
sloweyyy <truonglevinhphuc2006@gmail.com>
2024-09-18 01:10:42 +0700

    Add function to find uncommon words from two sentences

---
+++ Daily/884. Uncommon Words from Two Sentences.ts
@@ -0,0 +1,11 @@
+function uncommonFromSentences(s1: string, s2: string): string[] {
+    const words = (s1 + ' ' + s2).split(' ');
+    const wordCount = new Map<string, number>();
+    for (const word of words) {
+        wordCount.set(word, (wordCount.get(word) || 0) + 1);
+    }
+
+    return Array.from(wordCount.entries())
+        .filter(([word, count]) => count === 1)
+        .map(([word, _]) => word);
+};
