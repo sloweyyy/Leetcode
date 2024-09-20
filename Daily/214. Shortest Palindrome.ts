dbb62566850ecc0a73f86860232c13b568c232bc
sloweyyy <truonglevinhphuc2006@gmail.com>
2024-09-20 20:57:14 +0700

    feat: Add shortestPalindrome function

---
+++ Daily/214. Shortest Palindrome.ts
@@ -0,0 +1,19 @@
+function shortestPalindrome(s: string): string {
+    let i = 0;
+
+    for (let j = s.length - 1; j >= 0; j--) {
+        if (s[i] === s[j]) {
+            i++;
+        }
+    }
+
+    if (i === s.length) {
+        return s;
+    }
+
+    const suffix = s.slice(i);
+    const prefix = suffix.split('').reverse().join('');
+    const mid = shortestPalindrome(s.slice(0, i));
+
+    return prefix + mid + suffix;
+};
