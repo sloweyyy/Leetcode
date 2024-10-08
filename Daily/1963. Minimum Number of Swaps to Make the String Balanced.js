d3b8fc462a909342a29d7534a3386384287dbb96
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2024-10-08 15:55:32 +0700

    feat: Add minSwaps function for calculating the minimum number of swaps to make a string balanced

---
+++ Daily/1963. Minimum Number of Swaps to Make the String Balanced.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minSwaps = function(s) {
+    let stack = [];
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '[') {
+            stack.push('[');
+        } else {
+            if (stack.length > 0) {
+                stack.pop();
+            } else {
+                count++;
+            }
+        }
+    }
+    return Math.ceil(count / 2);
+};
