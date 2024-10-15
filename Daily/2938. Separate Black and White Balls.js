11409b5ca88b03a8fa64e3fa7082ef27269240b7
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2024-10-16 00:21:38 +0700

    feat: Add minimumSteps function for counting the minimum steps to separate black and white balls

---
+++ Daily/2938. Separate Black and White Balls.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumSteps = function(s) {
+  let ans = 0;
+  let blackCount = 0; 
+
+  for (let i = 0; i < s.length; i++) {
+      if (s[i] === '0') {
+          ans += blackCount;
+      } else {
+          blackCount++;
+      }
+  }
+  return ans;    
+};
