4165aec457440d558d24dc4815d555d2aa7551a0
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2024-10-20 03:32:38 +0700

    feat: Add findKthBit function for finding the kth bit in the nth binary string

---
+++ Daily/1545. Find Kth Bit in Nth Binary String.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {character}
+ */
+var findKthBit = function(n, k) {
+    let s = "0";
+    for (let i = 1; i < n; i++) {
+        s = s + "1" + s.split("").map(c => c == "0" ? "1" : "0").reverse().join("");
+    }
+    return s[k - 1];
+};
