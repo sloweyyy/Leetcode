0ccd52014fed35f4c9a21f91e2d1e6ce4200d52a
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2024-10-24 18:05:42 +0700

    feat: Add flipEquiv function for checking if two binary trees are flip equivalent

---
+++ Daily/951. Flip Equivalent Binary Trees.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function flipEquiv(root1: TreeNode | null, root2: TreeNode | null): boolean {
+    if (!root1 && !root2) return true;
+    if (!root1 || !root2) return false;
+    if (root1.val !== root2.val) return false;
+
+    return (
+        (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) ||
+        (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left))
+    );
+};
