e394fc6ea4ed30b8883928e664a2b1c1a01ad37f
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2024-10-25 09:18:28 +0700

    feat: Add removeSubfolders function for removing sub-folders from the filesystem

---
+++ Daily/1233. Remove Sub-Folders from the Filesystem.ts
@@ -0,0 +1,13 @@
+function removeSubfolders(folder: string[]): string[] {
+    folder.sort();
+    const result = [];
+    let prev = folder[0];
+    result.push(prev);
+    for (let i = 1; i < folder.length; i++) {
+        if (!folder[i].startsWith(prev + '/')) {
+            result.push(folder[i]);
+            prev = folder[i];
+        }
+    }
+    return result;  
+};
