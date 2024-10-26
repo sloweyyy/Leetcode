4fb33dabed9ef83816ce49a455c968b66dc85c80
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2024-10-26 23:01:24 +0700

    feat: Implement treeQueries function for calculating heights of binary tree after subtree removal queries

---
+++ Daily/2458. Height of Binary Tree After Subtree Removal Queries.ts
@@ -0,0 +1,48 @@
+function treeQueries(root: TreeNode | null, queries: number[]): number[] {
+  const heights: number[] = new Array(50000).fill(0);
+  const d: number[] = new Array(100001).fill(0);
+  const l: number[] = new Array(100001).fill(0);
+  const r: number[] = new Array(100001).fill(0);
+  let len: number = 0;
+  
+  function search(p: TreeNode, h: number): void {
+      d[p.val] = h;
+      
+      if (!p.left && !p.right) {
+          heights[len] = h;
+          l[p.val] = r[p.val] = len;
+          len++;
+          return;
+      }
+      
+      l[p.val] = len;
+      
+      if (p.left) search(p.left, h + 1);
+      if (p.right) search(p.right, h + 1);
+      
+      r[p.val] = len - 1;
+  }
+  
+  if (!root) return [];
+  search(root, 0);
+  
+  const n: number = len;
+  const maxl: number[] = new Array(n).fill(0);
+  const maxr: number[] = new Array(n).fill(0);
+  
+  for (let i = 1; i < n; i++) {
+      maxl[i] = Math.max(maxl[i-1], heights[i-1]);
+      maxr[n-i-1] = Math.max(maxr[n-i], heights[n-i]);
+  }
+  
+  const ret: number[] = [];
+  const k: number = queries.length;
+  
+  for (let i = 0; i < k; i++) {
+      const maxxl: number = maxl[l[queries[i]]];
+      const maxxr: number = maxr[r[queries[i]]];
+      ret.push(Math.max(Math.max(maxxl, maxxr), d[queries[i]] - 1));
+  }
+  
+  return ret;
+}
---
+++ Daily/2623. Memoize.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+  let cache = {};
+
+  return function(...args) {
+    let key = JSON.stringify(args);
+    if (cache[key]) {
+      return cache[key];
+    } else {
+      let result = fn(...args);
+      cache[key] = result;
+      return result;
+    }    
+  }
+}
+
+
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+ callCount += 1;
+  return a + b;
+})
+memoizedFn(2, 3) // 5
+memoizedFn(2, 3) // 5
+console.log(callCount) // 1 
