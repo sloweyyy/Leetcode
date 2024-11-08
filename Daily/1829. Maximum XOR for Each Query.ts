6ec3316be7629b5b1746680e3b2876d3e6d2d84e
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2024-11-08 21:35:19 +0700

    feat: Add solution for "Maximum XOR for Each Query" problem

---
+++ Daily/1829. Maximum XOR for Each Query.ts
@@ -0,0 +1,11 @@
+function getMaximumXor(nums: number[], maximumBit: number): number[] {
+    const n = nums.length;
+    const res = new Array(n);
+    let max = (1 << maximumBit) - 1;
+    let xor = 0;
+    for (let i = 0; i < n; i++) {
+        xor ^= nums[i];
+        res[n - i - 1] = max ^ xor;
+    }
+    return res;  
+};
