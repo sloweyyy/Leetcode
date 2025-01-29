8df7899a93ea9c92dc06af045c3ba2e6e01831eb
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2025-01-29 21:30:54 +0700

    feat: Add solution for Redundant Connection problem

---
+++ Daily/0684. Redundant Connection.java
@@ -0,0 +1,31 @@
+
+class Solution {
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        int[] parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int pu = find(parent, u);
+            int pv = find(parent, v);
+            if (pu == pv) {
+                return edge;
+            }
+            parent[pu] = pv;
+        }
+
+        return new int[0];
+    }
+
+    private int find(int[] parent, int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent, parent[x]);
+        }
+        return parent[x];
+    }
+}
