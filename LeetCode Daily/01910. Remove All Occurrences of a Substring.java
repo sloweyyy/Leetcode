ae18ede35b65dda906d4b3b7324bf132722bea2b
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2025-02-11 13:16:50 +0700

    feat: Add solution for "Remove All Occurrences of a Substring"

---
+++ LeetCode Daily/01910. Remove All Occurrences of a Substring.java
@@ -0,0 +1,10 @@
+
+class Solution {
+
+    public String removeOccurrences(String s, String part) {
+        while (s.contains(part)) {
+            s = s.replaceFirst(part, "");
+        }
+        return s;
+    }
+}
